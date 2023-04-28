9566eaaada8e55e2169572f138c8f5c0b5c87037
UrazbaevT <utilek990@gmail.com>
2023-04-28 19:18:12 +0300

    added code of the lesson2 & hw2 (passing data to another fragment + xml)

---
+++ app/src/main/java/com/example/lovecalculator_api/FirstFragment.kt
@@ -0,0 +1,58 @@
+package com.example.lovecalculator_api
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.example.lovecalculator_api.databinding.FragmentFirstBinding
+import retrofit2.Call
+import retrofit2.Response
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        with(binding) {
+            btnCalculate.setOnClickListener {
+                LoveService().api.calculatePercentage(
+                    firstName = firstEt.text.toString(),
+                    secondName = secondEt.text.toString()
+                ).enqueue(object : retrofit2.Callback<LoveModel> {
+                    override fun onResponse(call: Call<LoveModel>, response: Response<LoveModel>) {
+                        if (response.isSuccessful) {
+                            Log.e("ololo", "onResponse: ${response.body()}")
+                            findNavController().navigate(
+                                R.id.resultFragment,
+                                bundleOf("result" to response.body())
+                            )
+                        }
+                    }
+
+                    override fun onFailure(call: Call<LoveModel>, t: Throwable) {
+                        Log.e("ololo", "onFailure: ${t.message}")
+                    }
+
+                })
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/lovecalculator_api/LoveModel.kt
@@ -0,0 +1,19 @@
+package com.example.lovecalculator_api
+
+import com.google.gson.annotations.SerializedName
+
+//    {
+//        "fname": "Tilek",
+//        "sname": "Alice",
+//        "percentage": "75",
+//        "result": "All the best!"
+//    }
+
+data class LoveModel (
+    @SerializedName("fname")
+    val firstName: String,
+    @SerializedName("sname")
+    val secondName: String,
+    val percentage: String,
+    val result: String,
+    ):java.io.Serializable
